a9ab4816cd3c7bb3e38a0732e80f04a30e51d473
Oran <oran.luzon@gmail.com>
2015-05-14 12:12:03 -0400

    added MecanumDrive subsystem Skeleton

---
+++ src/org/usfirst/frc/team1155/robot/MecanumDrive.java
@@ -0,0 +1,14 @@
+package org.usfirst.frc.team1155.robot.subsystems;
+
+import org.usfirst.frc.team1155.robot.Hardware;
+import edu.wpi.first.wpilibj.CANTalon;
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+public class MecanumDrive extends Subsystem{
+  
+  public MecanumDrive(){
+  
+  }
+  
+  
+}
