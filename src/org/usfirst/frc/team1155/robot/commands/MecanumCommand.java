0a53104220472cbcb85a596e7fb36cf8eba139d1
luzono <luzono@013-23.bxsci.78x445.nycboe.org>
2015-05-18 16:36:14 -0400

    finished code

--- src/org/usfirst/frc/team1155/robot/commands/MecanumCommand.java
+++ src/org/usfirst/frc/team1155/robot/commands/MecanumCommand.java
@@ -1,6 +1,7 @@
 package org.usfirst.frc.team1155.robot.commands;
 import edu.wpi.first.wpilibj.CANTalon;
 import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.RobotDrive;
 import edu.wpi.first.wpilibj.command.Command;
 
 import org.usfirst.frc.team1155.robot.Hardware;
@@ -10,21 +11,25 @@ import org.usfirst.frc.team1155.robot.subsystems.MecanumDrive;
 public class MecanumCommand extends Command{
 	
 	private Joystick rotationJoystick, positionJoystick;
+	private RobotDrive mecanumDrive;
 	
 	public MecanumCommand(){
 		requires(Robot.mecanumDrive);
+		rotationJoystick = Hardware.INSTANCE.leftJoystick;
+		positionJoystick = Hardware.INSTANCE.rightJoystick;
+		mecanumDrive = new RobotDrive(1, 2, 3, 4);
 	}
 	
 	@Override
 	protected void initialize() {
 		// TODO Auto-generated method stub
-		
+		mecanumDrive.mecanumDrive_Cartesian(positionJoystick.getX(), positionJoystick.getY(), Robot.mecanumDrive.getRate(),Robot.mecanumDrive.getRotation());
 	}
 	
 	@Override
 	protected void execute() {
 		// TODO Auto-generated method stub
-		
+		Robot.mecanumDrive.rotateRobot(rotationJoystick.getX());
 	}
 	
 	@Override
--- src/org/usfirst/frc/team1155/robot/subsystems/MecanumDrive.java
+++ src/org/usfirst/frc/team1155/robot/subsystems/MecanumDrive.java
@@ -3,25 +3,40 @@ package org.usfirst.frc.team1155.robot.subsystems;
 import org.usfirst.frc.team1155.robot.Hardware;
 
 import edu.wpi.first.wpilibj.CANTalon;
+import edu.wpi.first.wpilibj.CANTalon.ControlMode;
 import edu.wpi.first.wpilibj.Gyro;
 import edu.wpi.first.wpilibj.command.Subsystem;
 
 public class MecanumDrive extends Subsystem{
 	
-  private CANTalon FrontLeftTalon, RearLeftTalon, FrontRightTalon, RearRightTalon;
-  public Gyro gyro;
+  private Gyro gyro;
+  private CANTalon frontLeftTalon, frontRightTalon, backLeftTalon, backRightTalon;
   
   public MecanumDrive(){
 	  //gyro = Hardware.INSTANCE.gyro;
+	  //gyro.reset();
 	  
-	  FrontLeftTalon = Hardware.INSTANCE.frontLeftTalon;
-	  RearLeftTalon = Hardware.INSTANCE.backLeftTalon;
-	  FrontRightTalon = Hardware.INSTANCE.frontRightTalon;
-	  RearRightTalon = Hardware.INSTANCE.backRightTalon;
+	  frontLeftTalon = Hardware.INSTANCE.frontLeftTalon;
+	  frontRightTalon = Hardware.INSTANCE.frontRightTalon;
+	  backLeftTalon = Hardware.INSTANCE.backLeftTalon;
+	  backRightTalon = Hardware.INSTANCE.backRightTalon;
   }
-  public static void set(double frontRightValue, double rearRightValue, double frontLeftValue){
-	  
+  
+  public int getRotation(){
+	  return 0; //gyro.getAngle();
   }
+  
+  public int getRate(){
+	  return 0; //gyro.getRate();
+  }
+  
+  public void rotateRobot(double speed){
+	  frontLeftTalon.set(speed);
+	  backLeftTalon.set(speed);
+	  frontRightTalon.set(-speed);
+	  backRightTalon.set(-speed);
+  }
+  
 @Override
 protected void initDefaultCommand() {
 	// TODO Auto-generated method stub
